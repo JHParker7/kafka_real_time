a4c08e45730c54d08d3a7e34af7249721fc75f49
JHParker7 <111579699+JHParker7@users.noreply.github.com>
2023-09-06 14:15:12 +0100

    cleaning

--- collector.py
+++ collector.py
@@ -12,18 +12,12 @@ consumer = KafkaConsumer(
     value_deserializer=lambda m: json.loads(m.decode('utf-8'))
 )
 
-x=0
 for message in consumer:
     print("message received")
     data=message.value
     data=pd.read_json(StringIO(data))
-
-  #  print(" ")
-   # print(data.iloc[0].to_list())
-
     #add proccessing here
     data=data.fillna("This is was a Null")
     print("message proccesed")
-   # print(data.iloc[0].to_list())
     data.to_sql("test",con=conn, if_exists="append",index=False)
     print("message added to DB")
--- data_generator.py
+++ data_generator.py
@@ -5,6 +5,7 @@ import numpy as np
 import sqlalchemy as sql
 import random
 import string
+import time
 
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9093'],
@@ -12,14 +13,8 @@ producer = KafkaProducer(
 )
 for x in range(100):
     df=pd.DataFrame([[np.random.randint(0,1000),np.random.randint(0,1000),np.random.randint(0,1000),np.random.randint(0,1000),np.random.randint(0,1000),np.random.randint(0,1000),np.random.randint(0,1000), random.choice(string.ascii_letters), random.choice(string.ascii_letters), random.choice(string.ascii_letters), random.choice(string.ascii_letters)+random.choice(string.ascii_letters)+ random.choice(string.ascii_letters),np.nan]])
-    print(df)
     producer.send('data',df.to_json())
 
+time.sleep(10)
 conn=sql.create_engine("postgresql+pg8000://postgres:postgres@localhost:5438")
-print("""
-
-
-
-
-""")
 print(pd.read_sql_table("test",con=conn))
